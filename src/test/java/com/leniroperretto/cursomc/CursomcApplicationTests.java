e584725a694bc75a2544c534aeddbd68c829ffe6
Leniro Perretto <leniroperretto@gmail.com>
2024-01-02 11:39:56 -0300

    Projeto criado

---
+++ src/test/java/com/leniroperretto/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leniroperretto.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
